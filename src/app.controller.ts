89edecb05fdae7e4d5b99d904fae3a1a1beaee74
Mario-Coxe <mario.coxe@ideiasdinamicas.com>
2025-03-02 02:09:01 +0100

    refactory: when registering use uuid and dto

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,19 +1,21 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 import { PrismaService } from './database/prisma.service';
+import CreateTeamMemberBody from './dtos/create-team-member-body';
+import { randomUUID } from 'node:crypto';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService, private prisma: PrismaService) {}
 
   @Get('hello')
-  async getHello() {
+  async getHello(@Body() body: CreateTeamMemberBody) {
 
     const member = await this.prisma.rocketTeamMember.create({
       data: {
-        id: "text",
-        name: "Coxe",
-        function: "Capitão"
+        id: randomUUID(),
+        name: body.name,
+        function: body.function
       }
     })
 
---
+++ src/dtos/create-team-member-body.ts
@@ -0,0 +1,5 @@
+export default  class CreateTeamMemberBody {
+  name: string
+  function: string
+}
+
